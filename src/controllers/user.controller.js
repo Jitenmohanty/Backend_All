cb4ed248ff6d826453e8f7b75b7f1eb4830f33f9
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-12-23 21:29:49 +0530

    Error Resolve on cookies set , in devlopement secure not required

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -123,7 +123,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const options = {
     httpOnly: true,
-    secure: false,
+    // secure: false,
   };
 
   return await res
@@ -158,7 +158,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 
   const options = {
     httpOnly: true,
-    secure: true,
+    // secure: true,
   };
 
   return res
